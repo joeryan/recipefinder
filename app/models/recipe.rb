f5ebb202826a376fb8b6d053ec88ac3ef6c08cb9
Joe Ryan <joe@clanryan.us>
2016-04-03 22:38:15 -0400

    fix query syntax in Recipe class

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,12 @@
 class Recipe
   include HTTParty
-  
+
   host_port = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{host_port}/api"
   default_params sort: 'r', key: ENV['FOOD2FORK_KEY']
   format :json
 
   def self.for term
-    get("/search", query: {query: term})["recipes"]
+    get("/search", query: {q: term})["recipes"]
   end
 end
